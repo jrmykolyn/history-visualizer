73833ef2d5807c7f67b920b68627a8abb59ed05b
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2018-12-13 19:43:55 -0500

    topic-3 Add CSS namespace class to top-level DOM node

--- src/tags/app/App.js
+++ src/tags/app/App.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import styles from './index.css';
 import { Modal, Stack } from '../components';
 
+export const CSS_NAMESPACE = 'hv';
+
 export class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={ CSS_NAMESPACE }>
         <Stack api={ this.props.api } />
         <Modal />
       </div>
