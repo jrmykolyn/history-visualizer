dec7e98f56172b015183988e998d86bbb5b20f46
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2018-12-18 08:40:28 -0500

    topic-11 Implement general action creator tests

--- test/state/actions/creators.test.js
+++ test/state/actions/creators.test.js
@@ -7,11 +7,24 @@ import { Actions as Types, ActionCreators as Actions } from '../../../src/state/
 // DEFINE TESTS
 // -------------------------------------------------- 
 describe('Action creators', () => {
-  describe('addFrame()', () => {
-    it('should return an action of the correct type', () => {
-      expect(Actions.addFrame()).to.eql({ type: Types.ADD_FRAME, payload: undefined });
-    });
+  describe('General', () => {
+    const dictionary = {
+      'addFrame': Types.ADD_FRAME,
+      'incrementCount': Types.INCREMENT_COUNT,
+      'selectFrame': Types.SELECT_FRAME,
+      'setCount': Types.SET_COUNT,
+      'toggleDrawer': Types.TOGGLE_DRAWER,
+      'toggleModal': Types.TOGGLE_MODAL,
+    };
 
+    Object.keys(dictionary).forEach((method) => {
+      it(`${method}() should return an action of the correct type`, () => {
+        expect(Actions[method]()).to.include({ type: dictionary[method] });
+      });
+    });
+  });
+
+  describe('addFrame()', () => {
     it('should accept a payload', () => {
       expect(Actions.addFrame('__FOO__')).to.eql({ type: Types.ADD_FRAME, payload: '__FOO__' });
     });
