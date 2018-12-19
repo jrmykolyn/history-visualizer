6a0d84bf40ea2a3b5b0058aad5ed32a97de4e6f4
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2018-12-19 08:41:40 -0500

    topic-13 Add tests for 'count' selector

---
+++ test/state/selectors/selectors.test.js
@@ -0,0 +1,17 @@
+import { it } from 'mocha';
+import { expect } from 'chai';
+
+import * as Selectors  from '../../../src/state/selectors';
+
+// --------------------------------------------------
+// DEFINE TESTS
+// --------------------------------------------------
+describe('Selectors', () => {
+  describe('count()', () => {
+    it('should return the count', () => {
+      const state = { frames: { count: 0 } };
+
+      expect(Selectors.count(state)).to.eql(0);
+    });
+  });
+});
